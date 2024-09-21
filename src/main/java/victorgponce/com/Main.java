f37a7f6b433e391989cdec78707cea596898cbdf
ponchisao326_ <44710450+ponchisao326@users.noreply.github.com>
2024-09-22 01:57:18 +0200

    Update Main.java

--- src/main/java/victorgponce/com/Main.java
+++ src/main/java/victorgponce/com/Main.java
@@ -1,7 +1,41 @@
 package victorgponce.com;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import victorgponce.com.listeners.WelcomeListener;
+import victorgponce.com.listeners.buttons;
+import victorgponce.com.listeners.info;
+import victorgponce.com.listeners.ping;
+
+import java.awt.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    // Introduce tu Token
+    static String token = "";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] fontNames = ge.getAvailableFontFamilyNames();
+
+        for (String fontName : fontNames) {
+            System.out.println(fontName);
+        }
+
+        JDA jda = JDABuilder.createDefault(token)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                .disableCache(CacheFlag.ACTIVITY).build();
+
+        jda.getPresence().setActivity(Activity.customStatus("Atrapando Autistas - Powered by PonchisaoHosting"));
+
+        // Añadir Listeners
+        jda.addEventListener(new listener());
+        jda.addEventListener(new ping());
+        jda.addEventListener(new buttons());
+        jda.addEventListener(new WelcomeListener());
     }
 }
